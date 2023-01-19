9710f07bc239ceb79b27f65414e1021410e9706d
faizamahmoud <faiza.mahmoud@gmail.com>
2023-01-19 11:08:22 -0700

    deploy v2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { render } from 'react-dom'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
+import React from 'react'
 
 render(
   <ThemeProvider>
