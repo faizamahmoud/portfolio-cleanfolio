1d11f4741722a7325020f482fe0082c81eda17e2
faizamahmoud <faiza.mahmoud@gmail.com>
2023-02-04 20:08:14 -0700

    add browser router'

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from './contexts/theme'
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
 import './index.css'
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
